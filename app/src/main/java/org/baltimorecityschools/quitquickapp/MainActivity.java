3ebc88d068fe669366c41563569bcaeced78d7c2
Anna24-bit <annael.green@gmail.com>
2025-03-19 14:54:43 -0400

    added images and spacing

--- app/src/main/java/org/baltimorecityschools/quitquickapp/MainActivity.java
+++ app/src/main/java/org/baltimorecityschools/quitquickapp/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     Intent goToset;
     Intent goTopro;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
