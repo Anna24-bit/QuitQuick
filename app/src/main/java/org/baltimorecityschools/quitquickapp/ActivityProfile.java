58bfeeb6301aaa96a01d6f7a8a934a5061a66bd6
Anna24-bit <annael.green@gmail.com>
2025-03-24 14:50:04 -0400

    new image

--- app/src/main/java/org/baltimorecityschools/quitquickapp/ActivityProfile.java
+++ app/src/main/java/org/baltimorecityschools/quitquickapp/ActivityProfile.java
@@ -1,6 +1,10 @@
 package org.baltimorecityschools.quitquickapp;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityProfile extends AppCompatActivity {
 
+    ImageView profile;
+    Button pBTN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+        profile = (ImageView)findViewById(R.id.imageView);
+        pBTN = (Button)findViewById(R.id.pButton);
 
+        pBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profile.setVisibility(View.VISIBLE);
+                pBTN.setVisibility(View.GONE);
+            }
+        });
     }
+
+
+
 }
