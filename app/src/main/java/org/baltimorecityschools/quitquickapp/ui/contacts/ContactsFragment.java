97b32fe09c0ba4366d167f83a87566c9357a90a4
Anna24-bit <annael.green@gmail.com>
2025-04-01 14:49:29 -0400

    added new contactts

---
+++ app/src/main/java/org/baltimorecityschools/quitquickapp/ui/contacts/ContactsFragment.java
@@ -0,0 +1,36 @@
+package org.baltimorecityschools.quitquickapp.ui.contacts;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.baltimorecityschools.quitquickapp.databinding.FragmentGalleryBinding;
+
+public class ContactsFragment extends Fragment {
+
+    private ContactsViewModel contactsViewModel;
+
+    public static ContactsFragment newInstance() {
+        return new ContactsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ContactsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
