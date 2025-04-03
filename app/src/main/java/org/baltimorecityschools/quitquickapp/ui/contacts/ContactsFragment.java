0303aac11d263f23b6aab55093d84458b21ef680
Anna24-bit <annael.green@gmail.com>
2025-04-03 14:50:28 -0400

    no chnages

--- app/src/main/java/org/baltimorecityschools/quitquickapp/ui/contacts/ContactsFragment.java
+++ app/src/main/java/org/baltimorecityschools/quitquickapp/ui/contacts/ContactsFragment.java
@@ -7,8 +7,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
 
 import org.baltimorecityschools.quitquickapp.databinding.FragmentGalleryBinding;
 
@@ -23,13 +25,13 @@ public class ContactsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        return inflater.inflate(b.layout.fragment_contacts, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ContactsViewModel.class);
+        contactsViewModel = new ViewModelProvider(this).get(ContactsViewModel.class);
         // TODO: Use the ViewModel
     }
 
